e6fdf036f7053f3c357bbf4115540976b0c0a28e
Jinaka <jinaka.ugochukwu@gmail.com>
2018-11-01 11:42:59 +0000

    Movie cover added #1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import moviecover from './denofthieves.jpg';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +8,14 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">goodbye</h1>
+          <img src={logo} className ="App-logo"/>
+          <h1 className="App-title">Hello</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          
         </p>
+        <img src={moviecover} className="MovieCover"/>
+
       </div>
     );
   }
