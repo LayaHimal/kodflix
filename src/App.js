e788501603aa3ad5d40bcb71f620d364382e21a4
Jinaka <jinaka.ugochukwu@gmail.com>
2018-11-05 12:56:39 +0000

    TV show titles replaced with images #2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import BM from './blackmirror.jpg'
+//eslint-disable-next-line
 import moviecover from './denofthieves.jpg';
 import './App.css';
 
@@ -8,31 +9,21 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className ="App-logo"/>
-          <h1 className="App-title">Hello</h1>
+<h1 className="App-title">Hello</h1>
         </header>
-        <p className="App-intro">
-          
-        </p>
-        
+       
+       
         <div className="container">
         
-          <div className="item"><h2>Black Mirror</h2></div>
+          <div className="item"><img src={moviecover} className="MovieCover"/></div>
           <div className="item"><h2>Breaking Bad</h2></div> 
           <div className="item"><h2>Death Note</h2></div> 
-          
         </div>
+
         <div className="container">
-          <div className="item"><h2>Game of Thrones</h2></div>
+
+          <div className="item"> <img src ={BM} /></div>
           <div className="item"><h2>The Walking Dead</h2></div> 
           <div className="item"><h2>The Wire</h2></div> 
 
-        </div>
-        
-
-      </div>
-    );
-  }
-}
-//<img src={moviecover} className="MovieCover"/>
-export default App;
+export default App
