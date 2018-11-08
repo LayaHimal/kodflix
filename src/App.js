b332bd5359d97bc0e0cc848f9aae373fe0b76402
Jinaka <jinaka.ugochukwu@gmail.com>
2018-11-08 15:40:52 +0000

    Titles of TV shows appear when images are hovered over #5

--- src/App.js
+++ src/App.js
@@ -13,31 +13,35 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
-        <div className="container">
+      <div className="container">
           <div className="item">
-            <img src={TW} alt="The Wire" />
-          </div>
+            <img src={TW} alt="The Wire" /> 
+            <div className ="overlay">The Wire</div>
+          </div>       
           <div className="item">
             <img src={GT} alt="Game Of Thrones" />
+            <div className ="overlay">Game Of Thrones</div>
           </div>
           <div className="item">
             <img src={TWD} alt="The Walking Dead" />
+            <div className ="overlay">The Walking Dead</div>
           </div>
         </div>
         <div className="container">
           <div className="item">
             <img src={BM} alt="Black Mirror" />
+            <div className ="overlay">Black Mirror</div>
           </div>
           <div className="item">
             <img src={BB} alt="Breaking Bad" />
+            <div className ="overlay">Breaking Bad</div>
           </div>
           <div className="item">
             <img src={DN} alt="Death Note" />
+            <div className ="overlay">Death Note</div>
           </div>
         </div>
-      </div>
-
+        </div>
     )
   }
 }
