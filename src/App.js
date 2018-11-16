667e7b7bf5bb336db874fac38067461f1b73aa31
Jinaka <jinaka.ugochukwu@gmail.com>
2018-11-16 12:06:04 +0000

    component created contains all the Movies

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import MovieCoverGallery from './MovieCoverGallery';
 import './App.css';
 
-
+class App extends Component{
 
   render() {
     return (
-      <div className="App">
+      <div className ="App">
        <MovieCoverGallery />
       </div>
     )
